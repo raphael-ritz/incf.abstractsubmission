efc1d74ff1000e1a674b30a464cba2ff9d91a1bd
ritz <ritz@049db14e-af93-4367-90ee-0106815540f1>
2010-12-10 11:05:32 +0000

    Browser view for uploading sample data
    git-svn-id: https://sesame.incf.ki.se/svn/incf/core/incf.abstractsubmission/trunk@3941 049db14e-af93-4367-90ee-0106815540f1

---
+++ incf/abstractsubmission/browser/upload.py
@@ -0,0 +1,24 @@
+from Products.Five.browser import BrowserView
+
+from incf.abstractsubmission.sampledata import ABSTRACTS
+ 
+
+class Upload(BrowserView):
+    """Support uploading of sample data"""
+
+    def upload(self):
+        """Upload sample data"""
+        
+        target = self.context
+        ids = target.contentIds()
+
+        for abstract in ABSTRACTS:
+            id = abstract.get('id', '')
+            if not id or id in ids:
+                continue
+            target.invokeFactory(type_name="Abstract",
+                                 id=id,
+                                 )
+            target[id].edit(abstract)
+            target[id].reindexObject()
+
