8e590b30edbda5150e4bb035cc7725f2f1bcc992
ritz <ritz@049db14e-af93-4367-90ee-0106815540f1>
2010-12-10 12:23:10 +0000

    More sample data
    git-svn-id: https://sesame.incf.ki.se/svn/incf/core/incf.abstractsubmission/trunk@3947 049db14e-af93-4367-90ee-0106815540f1

--- incf/abstractsubmission/sampledata/abstracts.py
+++ incf/abstractsubmission/sampledata/abstracts.py
@@ -37,9 +37,80 @@ Technically, the INCF Software Center is implemented on top of a modern, flexibl
 </p><p>
 The INCF Secretariat hereby also would like to invite developers of software tools within the field of computational neuroscience to use the Software Center. A limited start-up help will be provided. Up-to-date information will be available at the poster.</p>""",
      'abstract_format': 'text/html',
+     'presentationFormat': 'Demo',
+     'whyDemo': 'We want to user acceptance teting at the meeting',
+     'topic': 'Infrastructural and portal services',
+     'sessionType': 'Demo Session',
+     },
+#
+    {'id': 'bjaalie',
+     'title': 'The International Neuroinformatics Coordination Facility',
+     'authors': [{'firstnames': 'Jan G',
+                  'lastname': 'Bjaalie',
+                  'email': 'jan.bjaalie@incf.org',
+                  'affiliation': 'INCF',
+                  'country': 'Sweden',
+                  },
+                 {'firstnames': 'Sten',
+                  'lastname': 'Grillner',
+                  'email': 'grillner@ki.se',
+                  'affiliation': 'Karolinska Institutet',
+                  'country': 'Sweden',
+                  },
+                 ],
+     'abstract': """<h2>What is the International Neuroinformatics Coordinating Facility?<br /></h2>
+<p>The INCF is a professional organization devoted to advancing the field of neuroinformatics. One of its aims is to develop an international neuroinformatics infrastructure, which promotes the sharing of data and computing resources to the international research community. <a title="Organization" class="internal-link" href="resolveuid/d8a9d98732506e2b156ef190c7118a7a">Learn more about the organization</a></p>
+<h2><img class="image-right" src="resolveuid/0e35be74c51c78151594f0ef70343363/image_preview" alt="Neuroinf_aboutINCF_2" /></h2>
+<h2>What is Neuroinformatics?</h2>
+<p>Neuroinformatics is a research field that encompasses the organization of neuroscience data and application of computational models and analytical tools.&nbsp;</p>
+<h2>How is the INCF funded?</h2>
+<p><span class="Apple-style-span"></span>INCF is funded by contributions from its <a title="INCF National Nodes" class="internal-link" href="resolveuid/dbcbf6e03ebe5e086af49d36a765c39d">member countries</a> and the US National Science Foundation (<a class="external-link" href="http://www.nsf.gov/">NSF</a>).&nbsp;</p>
+<h2>What is the purpose of the INCF?</h2>
+<p>The vision of the INCF is to catalyze and coordinate the global development of neuroinformatics in the scientific community.</p>
+<p>INCF has as its mission to foster scientific interaction through information flow within its global network; valuating neuroinformatics activities and infrastructures; and to facilitate training in neuroinformatics.&nbsp;</p>
+<p>INCF has also identified its values, which include openness, neutrality and stability.&nbsp;</p>
+<h3>INCF aims to implement its Vision, Mission and Values through:</h3>
+<ul><li>Our Core Activites: <a title="INCF Programs" class="internal-link" href="resolveuid/1041063f49c6076966a7eb42f2fafee9">INCF Programs</a>,&nbsp;<a title="INCF Workshops" class="internal-link" href="resolveuid/800f60f4910af72a42bbe47a31503e04">Workshops</a>&nbsp;and the annual <a title="Congress" class="internal-link" href="resolveuid/c7f677d0a3b5cea152d077e202773d0b">Neuroinformatics Congress</a></li><li>Creating a worldwide network of people: <a title="People" class="internal-link" href="resolveuid/90be8d0d2a0bbf6916e9c0fc0f718bf4">The Neuroinformatics People Directory</a></li><li>A web portal in order to make resources easily accessible. The portal contains community developed <a title="Resources" class="internal-link" href="resolveuid/be7e59e5e51706689446d5cfe3b607cc">Research tools</a>, and Neuroinformatics related <a title="Events" class="internal-link" href="resolveuid/ef52ecb274a3e8813ccf8a9d5d834130">Events</a>, <a title="Training" class="internal-link" href="resolveuid/5f9a68b55ad301e225262221322ead37">Training</a>, <a title="Funding" class="internal-link" href="resolveuid/cadfe285ff864c18be0219d7a47bcdd9">Funding</a> and <a title="Job Board" class="internal-link" href="resolveuid/8cdd4811a20802cf9f3620eb136480b0">Jobs</a> <br /></li></ul>
+     """,
+     'abstract_format': 'text/html',
      'presentationFormat': 'Poster',
      'whyDemo': '',
      'topic': 'General neuroinformatics',
+     'sessionType': 'Poster Session',
+     },
+#
+    {'id': 'incfdai',
+     'title': 'The INCF Digital Atlasing Infrastructure',
+     'authors': [{'firstnames': 'Ilya',
+                  'lastname': 'Zaslavsky',
+                  'email': 'ilya@sdsc.edu',
+                  'affiliation': 'San Diego Supercomputing Center',
+                  'country': 'U.S.A.',
+                  },
+                 {'firstnames': 'Richard',
+                  'lastname': 'Baldock',
+                  'email': 'richard@univ.ed.ac.uk',
+                  'affiliation': 'Universtiy of Edinburgh',
+                  'country': 'U.K.',
+                  },
+                 {'firstnames': 'Rembrandt',
+                  'lastname': 'Bakker',
+                  'email': 'r.bakker@donders.ru.nl',
+                  'affiliation': 'Donders Institute for Brain, Cognition and Behavior, Radboud UMC Nijmegen',
+                  'country': 'The Netherlands',
+                  },
+                 {'firstnames': 'Michael',
+                  'lastname': 'Hawrylycz',
+                  'email': 'MikeH@alleninstitute.org',
+                  'affiliation': 'Allen Institute for Brain Science',
+                  'country': 'USA',
+                  },
+                 ],
+     'abstract': 'This package provides a Python API to the Digital Brain Atlasing web services provided by the International Neuroinformatics Coordinating Facility (INCF). To be useful and functional it requires a working internet connection at runtime. The details of the core webservices within the INCF Digital Atlasing Infrastructure (incf.dai) are available from the specification.',
+     'abstract_format': 'text/html',
+     'presentationFormat': 'Poster',
+     'whyDemo': '',
+     'topic': 'Digital atlasing',
      'sessionType': 'Poster Session',
      },
 #
