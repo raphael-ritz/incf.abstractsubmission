6fc1b7092cc542f2969fbc092bdac87e31fb5140
ritz <ritz@049db14e-af93-4367-90ee-0106815540f1>
2010-12-10 10:49:26 +0000

    Starting sample data
    git-svn-id: https://sesame.incf.ki.se/svn/incf/core/incf.abstractsubmission/trunk@3940 049db14e-af93-4367-90ee-0106815540f1

---
+++ incf/abstractsubmission/sample_data/abstracts.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+ABSTRACTS = [
+    {'id': 'lillberg',
+     'title': 'The INCF Software Center',
+     'authors': [{'firstnames': 'Ylva',
+                  'lastname': 'Lillberg',
+                  'email': 'ylva@incf.org',
+                  'affiliation': 'INCF',
+                  'country': 'Sweden',
+                  },
+                 {'firstnames': 'Jörgen',
+                  'lastname': 'Modin',
+                  'email': 'jorgen@webworks.se',
+                  'affiliation': 'webworks.se',
+                  'country': 'Sweden',
+                  },
+                 {'firstnames': 'Raphael',
+                  'lastname': 'Ritz',
+                  'email': 'raphae.ritz@incf.org',
+                  'affiliation': 'INCF',
+                  'country': 'Sweden',
+                  },
+                 ],
+     'abstract': """<h3>Background</h3>
+<p>
+The mission of the International Neuroinformatics Coordinating Facility (<a href="http://www.incf.org">INCF</a>) is to coordinate and foster international activities in neuroinformatics. In general, this includes combining neuroscience and informatics research to develop and apply advanced tools and approaches essential for a major advancement in understanding the structure and function of the brain [1,2].
+</p>
+<p>
+There are a significant number of resources available for neuroscientists today, yet they are not used as widely as they should because discovering their existence and evaluating their quality and relevance remain tedious tasks. Furthermore, the development of such resources often relies on isolated laboratories where collaboration across projects would be beneficial. INCF has therefore created a Neuroinformatics Portal, and released a Software Center as its first component.
+</p>
+<h3>The INCF Software Center</h3>
+<p>
+The INCF Software Center facilitates the initiatives from the neuroinformatics community in the following way: (i) It strives to become the number one entry point for people in the field looking for neuroscience specific or supporting software. It offers advanced classification and search tools as well as rating and evaluation facilities. (ii) It provides modelers and tool developers within the community with up-to-date, web-based support services for software development including software publishing, release management, and community building, e. g. via issue trackers, help desks, wikis, etc. In addition, these services are meant to ensure the sustainability of the development projects, a problem commonly faced by projects with a predefined life-span.
+</p><p>
+Technically, the INCF Software Center is implemented on top of a modern, flexible and easily extensible web-based content management system (Plone). It is developed, maintained and hosted by the INCF Secretariat in Stockholm, Sweden. The INCF Software Center is available online at <a href="http://software.incf.org">software.incf.org</a>.
+</p><p>
+The INCF Secretariat hereby also would like to invite developers of software tools within the field of computational neuroscience to use the Software Center. A limited start-up help will be provided. Up-to-date information will be available at the poster.</p>""",
+     'abstract_format': 'text/html',
+     'presentationFormat': 'Poster',
+     'whyDemo': '',
+     'topic': 'General neuroinformatics',
+     'sessionType': 'Poster Session',
+     },
+#
+    {'id': '',
+     'title': '',
+     'authors': [{'firstnames': '',
+                  'lastname': '',
+                  'email': '',
+                  'affiliation': '',
+                  'country': '',
+                  },
+                 ],
+     'abstract': '',
+     'abstract_format': 'text/html',
+     'presentationFormat': 'Poster',
+     'whyDemo': '',
+     'topic': 'General neuroinformatics',
+     'sessionType': 'Poster Session',
+     },
+]
