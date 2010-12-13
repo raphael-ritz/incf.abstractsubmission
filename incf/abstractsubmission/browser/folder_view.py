8249b072777214c429d4d76407b24de79e1811d6
ritz <ritz@049db14e-af93-4367-90ee-0106815540f1>
2010-12-13 12:34:54 +0000

    Make topical listing more flexible: allow selective expansion of individual topics
    git-svn-id: https://sesame.incf.ki.se/svn/incf/core/incf.abstractsubmission/trunk@3956 049db14e-af93-4367-90ee-0106815540f1

--- incf/abstractsubmission/browser/folder_view.py
+++ incf/abstractsubmission/browser/folder_view.py
@@ -8,6 +8,12 @@ class FolderView(BrowserView):
     def catalog(self):
         return getToolByName(self.context, 'portal_catalog')
     
+    @property
+    def isAnonymous(self):
+        """True is user is anonymous False otherwise"""
+        membertool = getToolByName(self.context, 'portal_membership')
+        return membertool.isAnonymousUser()
+
     def getAbstractsByTopic(self, topic):
         """All abstracts for a given topic"""
         return self.catalog(portal_type="Abstract", 
@@ -15,8 +21,16 @@ class FolderView(BrowserView):
                             sort_on="created",
                             )   # XXX maybe add a path constraint?
 
-    @property
-    def isAnonymous(self):
-        """True is user is anonymous False otherwise"""
-        membertool = getToolByName(self.context, 'portal_membership')
-        return membertool.isAnonymousUser()
+# XXX should we @memoize this?
+    def getTopicCount(self, topic):
+        """number of abstracts for the topic given"""
+
+        return len(self.catalog(portal_type="Abstract", 
+                                Subject=topic,
+                                )
+                   )
+
+
+    def headline(self, topic):
+        """Name of topic followed by abstract count in parentheses"""
+        return "%s (%s)" % (topic, self.getTopicCount(topic))
