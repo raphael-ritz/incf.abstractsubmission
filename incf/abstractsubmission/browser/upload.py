e093e7edc4e5220578dcf1886f600bfaafd854fe
ritz <ritz@049db14e-af93-4367-90ee-0106815540f1>
2010-12-13 15:22:07 +0000

    Uploading sample data now produces 120 abstracts
    git-svn-id: https://sesame.incf.ki.se/svn/incf/core/incf.abstractsubmission/trunk@3962 049db14e-af93-4367-90ee-0106815540f1

--- incf/abstractsubmission/browser/upload.py
+++ incf/abstractsubmission/browser/upload.py
@@ -1,3 +1,6 @@
+import random
+import transaction
+
 from Products.Five.browser import BrowserView
 
 from incf.abstractsubmission.sampledata import ABSTRACTS
@@ -22,3 +25,33 @@ class Upload(BrowserView):
             target[id].edit(**abstract)
             target[id].reindexObject()
 
+        # to fully populate repeat the above but with
+        # random permutations of autors and random
+        # choice of topic
+
+        topics = target.getTopics()
+
+        for i in range(40):
+            ids = target.contentIds()
+            for abs in ABSTRACTS:
+                # work on copies - we are going to change it
+                abstract = abs.copy()
+                id = abstract.get('id', '')
+                if id is '':
+                    continue
+                id = '%s-%d' % (id, i+1)
+                abstract['id'] = id   # write back to avoid renaming later
+                abstract['topic'] = random.choice(topics)
+                authors = abstract['authors']
+                random.shuffle(authors)
+                abstract['authors'] = authors            
+                if id in ids:
+                    print "Found %s - doing nothing" % id
+                    continue
+                print "Creating %s" % id
+                target.invokeFactory(type_name="Abstract",
+                                     id=id,
+                                     )
+                target[id].edit(**abstract)
+                target[id].reindexObject()
+            transaction.savepoint(optimistic=True)
