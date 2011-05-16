9a71d4b662b0c9d805e2e06b36289ba57b8707ab
ritz <ritz@049db14e-af93-4367-90ee-0106815540f1>
2011-05-16 19:30:00 +0000

    Initial draft of notification script. Not yet tested.
    git-svn-id: https://sesame.incf.ki.se/svn/incf/core/incf.abstractsubmission/trunk@4481 049db14e-af93-4367-90ee-0106815540f1

---
+++ incf/abstractsubmission/profiles/default/workflows/submission_workflow/scripts/notify.py
@@ -0,0 +1,41 @@
+## Script (Python) "notify"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=state_change
+##title=
+##
+object = state_change.object
+mailhost = object.mailhost
+
+user = context.portal_membership.getMemberById(object.Creator())  # is Creator the right thing to use here?
+
+if user:
+    user_email = user.getProperty('email')
+    from_email = object.portal_url.getPortalObject().getProperty('email_from_address')
+    from_name = object.portal_url.getPortalObject().getProperty('email_from_name')
+    
+    subject = "Your submission to neuroinformatics 2011 has been accepted"
+    
+    name = user.getProperty('fullname')
+    message = "Hello " + name
+    message += '\n'
+    message += "Your contribution " 
+    message += object.Title()
+    message += " has been accepted for presentation at the INCF Neuroinformatics Congress 2011. "
+    message += '\n'
+    message += object.absolute_url()
+    message += '\n'
+    message += """
+    
+    Kind regards, 
+    """
+    message += from_name
+    
+    mailhost.secureSend(message, user_email, from_email, subject = subject)
+
+else:
+    print "No user found"
+    return printed
