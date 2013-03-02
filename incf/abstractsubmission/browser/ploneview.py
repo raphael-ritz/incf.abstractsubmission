1ce3056b81708d95264dc4af451d2997c33829d6
Raphael Ritz <raphael.ritz@gmail.com>
2013-03-02 09:37:38 +0100

    Make the override for showEditableBorder more generic.

--- incf/abstractsubmission/browser/ploneview.py
+++ incf/abstractsubmission/browser/ploneview.py
@@ -11,26 +11,18 @@ class IncfPlone(Plone):
 
     def showEditableBorder(self):
         """Determine if the editable border should be shown - overridden 
-        to catch our exceptions
+        to suppress it for regular users on submission folders.
+        Since people may add content it would show up otherwise.
         """
 
-        exceptions = ['abstracts',
-                      'proposals',
-                      ]
-
-        exception = False
-
         context = aq_inner(self.context)
-        context_url = context.absolute_url()
+        if context.getPortalTypeName() != 'SubmissionFolder':
+            return Plone.showEditableBorder(self)
 
-        for e in exceptions:
-            if context_url.endswith(e):
-                exception = True
-        
         portal_membership = getToolByName(context, 'portal_membership')
         checkPerm = portal_membership.checkPermission
 
-        if exception and not checkPerm('Review portal content', context):
+        if not checkPerm('Review portal content', context):
             return False
 
         return Plone.showEditableBorder(self)
